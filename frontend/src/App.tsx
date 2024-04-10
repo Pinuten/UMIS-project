32fa4a7cdd00a6328d1b9c9675ae031a76ffb38a
Axel Petersson <axel.818@gmail.com>
2024-04-10 14:20:24 +0200

    frontend fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 // src/App.tsx
-import React from 'react';
 import MatladorList from './components/MatladorList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Any other content */}
       </header>
       <main>
         <MatladorList />
