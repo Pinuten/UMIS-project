ca5cd16f15113cb7be3d757164fbd8ba839c8ede
Axel Petersson <axel.818@gmail.com>
2024-04-11 22:59:27 +0200

    styles login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,30 +3,39 @@ import MatladorList from './components/MatladorList';
 import StatsTracker from './components/StatsTracker';
 import Slide from './components/Slide';
 import './App.css'
+import { LoginButton } from './components/Login';
+import { useAuth0 } from '@auth0/auth0-react';
+import fridge from './assets/Fridge.png';
+
 
 const App: React.FC = () => {
+  const { isAuthenticated } = useAuth0();
   const refreshMatlador = useCallback(() => {
   }, []);
 
   return (
-    <body>
-     
-
-      <div className='app-container'>
-        <div className="headerContainer">
-          <Slide />
-          <h1 className='text-container'><em>Matlådor</em></h1>
-        </div>
-
-        <MatladorList />
-        <StatsTracker />
-
+    <div className='app-container'>
+      <div className="headerContainer">
+        <h1 className='text-container'><em>Matlådor</em></h1>
+        <LoginButton />
       </div>
-    </body>
 
+      {isAuthenticated ? (
+        <>
+          <Slide />
 
-
+          <MatladorList />
+          <StatsTracker />
+        </>
+      ) : (
+        <>
+        <h3><strong>Matlåda (n.)</strong> [<em>matˈloːda</em>] <strong>Swedish</strong><br />Swedish for lunch box</h3>
+        <img src={fridge} alt="fridge" />
+        </>
+      )}
+    </div>
   );
 };
 
 export default App;
+
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
 
-const AuthenticationButton: React.FC = () => {
+export const LoginButton: React.FC = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   
     if (!isAuthenticated) {
@@ -9,4 +9,6 @@ const AuthenticationButton: React.FC = () => {
   
     return <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>;
   };
+
+  
   
