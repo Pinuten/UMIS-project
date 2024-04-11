65386861b7ebc75567a45f06392cb379082974db
Axel Petersson <axel.818@gmail.com>
2024-04-11 14:46:17 +0200

    puts all endpoints behind [Authorize]

--- backend/Controllers/MatladaController.cs
+++ backend/Controllers/MatladaController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +21,7 @@ namespace backend.Controllers
         }
 
         // GET: api/Matlada
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Matlada>>> GetMatlada()
         {
@@ -27,6 +29,7 @@ namespace backend.Controllers
         }
 
         // GET: api/Matlada/5
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<Matlada>> GetMatlada(int id)
         {
@@ -42,6 +45,7 @@ namespace backend.Controllers
 
         // PUT: api/Matlada/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Authorize]
         [HttpPut("{id}")]
         public async Task<IActionResult> PutMatlada(int id, Matlada matlada)
         {
@@ -73,6 +77,7 @@ namespace backend.Controllers
 
         // POST: api/Matlada
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> AddMatlada([FromBody] inputModel inputModel)
         {
@@ -93,6 +98,7 @@ namespace backend.Controllers
 
         }
         // DELETE: api/Matlada/5
+        [Authorize]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteMatlada(int id)
         {
@@ -107,6 +113,7 @@ namespace backend.Controllers
 
             return NoContent();
         }
+        [Authorize]
         [HttpPost("Eaten/{id}")]
         public async Task<IActionResult> EatMatlada(int id)
         {
@@ -130,6 +137,7 @@ namespace backend.Controllers
 
             return NoContent();
         }
+        [Authorize]
         [HttpGet("Eaten")]
         public async Task<int> GetEaten()
         {
